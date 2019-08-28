c38e55f229ddd50cff7e6da54b0e0dd3f73cb987
Edmer <edmer.alarte@bigbenta.com>
2019-08-29 02:57:31 +0900

    check

--- app/src/main/java/com/app/farm/farmapp/Activity/MainNavActivity.kt
+++ app/src/main/java/com/app/farm/farmapp/Activity/MainNavActivity.kt
@@ -1,33 +1,28 @@
-package com.app.farm.farmapp
+package com.app.farm.farmapp.Activity
 
-import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.OrientationHelper
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
-import android.widget.Toast
-import com.bumptech.glide.Glide
+import com.app.farm.farmapp.*
+import com.app.farm.farmapp.Adapter.MessagesAdapter
+import com.app.farm.farmapp.Adapter.PicsAdapter
+import com.app.farm.farmapp.ViewHolder.RecyclerViewHolder
+import com.app.farm.farmapp.ViewHolder.RecyclerViewHolder2
 import com.google.gson.GsonBuilder
-import kotlinx.android.synthetic.main.activity_main_nav.*
-import kotlinx.android.synthetic.main.list_item_chat2.*
-import kotlinx.android.synthetic.main.list_item_chat2.view.*
 import okhttp3.*
 import java.io.IOException
 
 
-class MainNavActivity : AppCompatActivity() ,RecyclerViewHolder.ItemClickListener, RecyclerViewHolder2.ItemClickListener{
+class MainNavActivity : AppCompatActivity() , RecyclerViewHolder.ItemClickListener, RecyclerViewHolder2.ItemClickListener2 {
 
 
-
-
-
-    private var messageFeed: MainNavActivity.MessagesFeed? = null
+    private var messageFeed: MessagesFeed? = null
     private var recyclerview: RecyclerView? = null
     private var recyclerview2: RecyclerView? = null
     private var messagesadapter: MessagesAdapter? = null
@@ -35,7 +30,7 @@ class MainNavActivity : AppCompatActivity() ,RecyclerViewHolder.ItemClickListene
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_messages)
+        setContentView(R.layout.activity_messages)
 
         recyclerview2 = findViewById(R.id.recyclerView2) as RecyclerView
         recyclerview = findViewById(R.id.recyclerView) as RecyclerView
@@ -45,13 +40,14 @@ class MainNavActivity : AppCompatActivity() ,RecyclerViewHolder.ItemClickListene
         fetchJsonForHorizontalRecyclerView()
         fetchJsonForVerticalRecyclerView()
 
-
+        // Refreshes every 60 seconds
         val handler = Handler()
         val runnableCode = object : Runnable {
-            override fun run() {Log.d("Handlers", "Called on main thread")
+            override fun run() {
 
+                Log.d("Handlers", "Called on main thread")
                 fetchJsonForHorizontalRecyclerView()
-                handler.postDelayed(this, 60000)
+                handler.postDelayed(this, 60000)//60 seconds
             }
         }
 
@@ -61,9 +57,7 @@ class MainNavActivity : AppCompatActivity() ,RecyclerViewHolder.ItemClickListene
     fun fetchJsonForHorizontalRecyclerView() {
         println("Attempting to Fetch JSON")
 
-        val url = "https://lebsysco.000webhostapp.com/showallPics.php"
-
-        val request = Request.Builder().url(url).build()
+        val request = Request.Builder().url(Constants.url.plus(Constants.show_all_pics)).build()
 
         val client = OkHttpClient()
         client.newCall(request).enqueue(object: Callback {
@@ -71,23 +65,23 @@ class MainNavActivity : AppCompatActivity() ,RecyclerViewHolder.ItemClickListene
                 val body = response?.body()?.string()
 
                 println("Successful request pic $body")
-                println(body)
 
                 val gson = GsonBuilder().create()
 
-                messageFeed = gson.fromJson(body, MainNavActivity.MessagesFeed::class.java)
+                messageFeed = gson.fromJson(body, MessagesFeed::class.java)
                 println("gson request $messageFeed")
-                picsAdapter = PicsAdapter(this@MainNavActivity!!, this@MainNavActivity!!,messageFeed!!)
+                picsAdapter =
+                    PicsAdapter(
+                        this@MainNavActivity!!,
+                        this@MainNavActivity!!,
+                        messageFeed!!
+                    )
 
                 this@MainNavActivity!!.runOnUiThread(
                     {
                         recyclerview2!!.adapter = picsAdapter
 
                     })
-
-
-
-
 
             }
 
@@ -101,9 +95,7 @@ class MainNavActivity : AppCompatActivity() ,RecyclerViewHolder.ItemClickListene
     fun fetchJsonForVerticalRecyclerView() {
         println("Attempting to Fetch JSON")
 
-        val url = "https://lebsysco.000webhostapp.com/showall.php"
-
-        val request = Request.Builder().url(url).build()
+        val request = Request.Builder().url(Constants.url.plus(Constants.show_all)).build()
 
         val client = OkHttpClient()
         client.newCall(request).enqueue(object : Callback {
@@ -111,13 +103,16 @@ class MainNavActivity : AppCompatActivity() ,RecyclerViewHolder.ItemClickListene
                 val body = response?.body()?.string()
 
                 println("Successful request $body")
-                println(body)
 
                 val gson = GsonBuilder().create()
 
-                messageFeed = gson.fromJson(body, MainNavActivity.MessagesFeed::class.java)
+                messageFeed = gson.fromJson(body, MessagesFeed::class.java)
                 println("gson request $messageFeed")
-               messagesadapter = MessagesAdapter(this@MainNavActivity!!, this@MainNavActivity,messageFeed!!)
+               messagesadapter =
+                   MessagesAdapter(
+                       this@MainNavActivity!!, this@MainNavActivity,
+                       messageFeed!!
+                   )
 
                 this@MainNavActivity!!.runOnUiThread(
                     {
@@ -133,13 +128,19 @@ class MainNavActivity : AppCompatActivity() ,RecyclerViewHolder.ItemClickListene
         })
     }
 
+   //interface method Horizontal RecyclerView
     override fun onItemClick(view: View, position: Int) {
 
         val intent = Intent(this, Activity_WebV::class.java)
         intent.putExtra("Position", position)
         startActivity(intent)
     }
+    //interface method Vertical RecyclerView
+    override fun onItemClick2(view: View, position: Int) {
+        Log.d("onItemClick2", "POS $position")
+    }
 
+    //Data Model Classes
     class MessagesFeed(val post: List<Post>)
 
     class Post(val wall_id: Int, val post_pic: String, val caption: String, val post_date: String, val name: String)
